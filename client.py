a28ba343161c89210e9bf2680ab036e36298529f
Diego Celorio <diegocel@colostate.edu>
2024-10-22 15:24:29 -0600

    Adding client and server message handling

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import selectors
+import json
 
 sel = selectors.DefaultSelector()
 
@@ -21,6 +22,15 @@ try:
 
     sel.register(client, selectors.EVENT_READ, selectors.EVENT_WRITE)
 
+    player_name = input("Enter player name: ")
+    join_message = json.dumps({
+        "type":"join",
+        "payload": {
+            "player_name":player_name
+        }
+    })
+    client.send(join_message.encode())
+
     while True:
         events = sel.select(timeout=1)
 
@@ -30,9 +40,9 @@ try:
                     response = client.recv(BUFFER_SIZE)
 
                     if response:
-                        print(f"[Server]: {response}")
+                        handle_server_response(response.decode())
                     else:
-                        print("Connection closed")
+                        print("[-] Connection closed")
                         sel.unregister(client)
                         client.close()
                         sys.exit(0)
@@ -44,12 +54,31 @@ try:
                     sys.exit(1)
             
             if mask & selectors.EVENT_WRITE:
-                message = input("Type in message or use 'exit' to quit: ")
+                message = input("Enter 'move', 'chat' or use 'exit' to quit: ")
                 if message.lower() == 'exit':
                     print("Client exiting")
                     sel.unregister(client)
                     client.close()
                     sys.exit(0)
+                elif message.lower() == 'move':
+                    player_move = input("Call your shot! (x,y): ").split(',')
+                    move_message = json.dumps({
+                        "type":"move",
+                        "payload": {
+                            "position" : {"x": int(player_move[0]), "y": int(player_move[1])}
+                        }
+                    })
+                    client.send(move_message.encode())
+                elif message.lower() == 'chat':
+                    player_chat = input("Enter chat:")
+                    chat_message = json.dumps({
+                        "type":"chat",
+                        "payload": {
+                            "message": player_chat
+                        }
+                    })
+                    client.send(chat_message.encode())
+                
                 client.sendall(message.encode())
 
 except ConnectionRefusedError:
@@ -64,3 +93,14 @@ except Exception as e:
 finally:
     print(f"[*] Disconnected from {host}:{port}")
     client.close()
+
+def handle_server_response(response):
+    data = json.loads(response)
+    msg_type = data.get('type')
+    if msg_type == 'status':
+        print(f"Game Status: {data['payload']['message']}")
+        print("Players: ")
+        for player in data['payload']['players']:
+            print(f"- {player['player name']} at {player['position']}")
+    else:
+        print("Unknown message received")
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import logging
 import selectors
 import socket
 import sys
+import json
 
 
 logging.basicConfig(level=logging.INFO)
@@ -11,6 +12,7 @@ host = sys.argv[1]
 port = int(sys.argv[2])
 address = (host, port)
 BUFFER_SIZE = 1024
+clients = {}
 
 if len(sys.argv) != 3:
     print("[-] Usage:", sys.argv[0], "<host> <port>")
@@ -39,6 +41,63 @@ def close_client(client_socket):
     client_socket.close()
     print(f"[*] Client disconnected")
 
+    for client_id, info in list(clients.items()):
+        if info['socket'] == client_socket:
+            del clients[client_id]
+            broadcast_status(f"Player {info['name']} left the game.")
+            break
+
+def handle_message(client_socket, message):
+    try:
+        data = json.loads(message.decode())
+        msg_type = data.get('type')
+        payload = data.get('payload', {})
+
+        if msg_type == 'join':
+            handle_join(client_socket, payload)
+        elif msg_type == 'move':
+            handle_move(client_socket, payload)
+        elif msg_type == 'chat':
+            handle_chat(client_socket, payload)
+        elif msg_type == 'quit':
+            close_client(client_socket)
+    except:
+        print(f"[*] Invalid message from client try: 'join', 'move, 'chat' or use 'quit' to exit.")
+
+def handle_join(client_socket, payload):
+    player_name = payload.get('player_name')
+    client_id = len(clients) + 1
+    clients[client_id] = {'name': player_name}
+    print(f"[+] Player {player_name} joined the game")
+    broadcast_status(f"Player {player_name} joined the game")
+
+def handle_move(client_socket, payload):
+    player_move = payload.get('position')
+    for client_id, info in clients.items():
+        if info['socket'] == client_socket:
+            info['position'] = player_move
+            print(f"[+] Player {info['name']} striked {player_move}")
+            broadcast_status(f"Player {info['name']} striked {player_move}")
+            break
+
+def handle_chat(client_socket, payload):
+    player_chat = payload.get('message')
+    for client_id, info in clients.items():
+        if info['socket'] == client_socket:
+            print(f"[*] Player {info['name']} said {player_chat}")
+            broadcast_status(f"Player {info['name']} : {player_chat}")
+            break
+
+def broadcast_status(status_message):
+    status = json.dumps({
+        "type": "status",
+        "payload": {
+            'players': [{'player_name': info['name'], 'position': info['position']} for info in clients.values()],
+            'message': status_message
+        }
+    })
+    for client in clients.values():
+        client['socket'].send(status.encode())
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
